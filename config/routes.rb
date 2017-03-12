0538a7715dc50b573ae46c5d6e37fd2d34823548
samer <mrsamer123@hotmail.com>
2017-03-12 09:03:48 +0000

    Added routes for reviews and review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :plays
+  resources :plays do
+    resources :reviews
+  end
   
   root 'plays#index'
 end
